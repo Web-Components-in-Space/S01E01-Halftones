70011c54187be2a178268d43c86e118f7216d30c
Ben Farrell <ben@benfarrell.com>
2021-12-10 20:44:19 -0800

    step 1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # S01E01-Halftones
-Halftone images
+
+# Step 1: Setup
+Since this is the first episode and the first time we're setting up a lit application,
+lets focus on the first steps of laying out our application.
+
+In this first step, we cover:
+
+- Creating our gitignore and putting our assets into place
+- Creating a package.json and installing our dependencies
+- Creating our application source folder with a single component folder
+- Creating our index.html and pointing to our app component source
+- Creating our first component file (app.js) with an index.js entry point
+- Fill in our component with Lit functionality and render some HTML
+- Import Spectrum Web Components and add a theme container
+- Build out our layout using HTML and CSS
+- Get the halftone component working
+- Add a background slot
+
+
+
--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,55 @@
-import {LitElement} from 'lit';
-import {template} from './app.html';
-import {style} from './app.css';
+import {LitElement, html, css} from 'lit';
+import 'halftone-web-component';
 
 export class App extends LitElement {
     static get styles() {
-        return [style];
+        return [
+            css` 
+                :host {
+                  height: 100%;
+                  width: 100vw;
+                }
+                
+                sp-theme {
+                  width: 100%;
+                  height: 100%;
+                  display: flex;
+                }
+                
+                #main-container {
+                  width: 100%;
+                  display: flex;
+                  flex-direction: column;
+                }
+                
+                halftone-svg {
+                  display: inline-block;
+                  width: 100%;
+                  height: 100%;
+                }
+                
+                #background {
+                  width: 100%;
+                  height: 100%;
+                }`
+        ];
     }
 
     render() {
-        return template(this);
+        return html`<sp-theme scale="medium" color="light">
+            <ht-sidepanel style="background-color: blue; width: 250px; height: 100%;"></ht-sidepanel>
+            <div id="main-container" style="background-color: red">
+                <ht-floatingheader style="background-color: yellow; width: 100%; height: 100px;"></ht-floatingheader>
+                <halftone-svg style="width: 100%; height: 100%"
+                              src="./assets/astronaut.jpg"
+                              distance="14"
+                              shapecolor="white"
+                              shapetype="circles">
+                    <div id="background" style="background-color: black"></div>
+                </halftone-svg>
+                <ht-floatingfooter style="background-color: purple; width: 100%; height: 100px;"></ht-floatingfooter>
+            </div>
+        </sp-theme>`;
     }
 }
 
