7fc0ec8dc3fb5aaf8401e3bcbe548edb102434e4
Ben Farrell <ben@benfarrell.com>
2021-12-12 16:41:22 -0800

    don't need width and height on halftone component

--- src/app/app.html.js
+++ src/app/app.html.js
@@ -10,11 +10,11 @@ export const template = function(scope) { return html`
         <ht-sidepanel style="background-color: blue; width: 250px; height: 100%;"></ht-sidepanel>
         <div id="main-container">
             <ht-floatingheader></ht-floatingheader>
-            <halftone-svg style="width: 100%; height: 100%"
-                          src="./assets/astronaut.jpg"
-                          distance="14"
-                          shapecolor="white"
-                          shapetype="circles">
+            <halftone-svg
+                  src="./assets/astronaut.jpg"
+                  distance="14"
+                  shapecolor="white"
+                  shapetype="circles">
                 <div id="background" style="background-color: black"></div>
             </halftone-svg>
             <ht-floatingfooter style="background-color: purple; width: 100%; height: 100px;"></ht-floatingfooter>
