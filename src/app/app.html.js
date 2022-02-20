349ed7d176a17ac6fc082f379aafa80f33ee7099
Ben Farrell <ben@benfarrell.com>
2022-02-20 15:14:03 -0800

    don't use sp-theme quite yet, and don't add the background quite yet

--- src/app/app.html.js
+++ src/app/app.html.js
@@ -1,12 +1,9 @@
 import {html} from 'lit';
-import '@spectrum-web-components/theme/sp-theme';
-import '@spectrum-web-components/theme/theme-light';
-import '@spectrum-web-components/theme/scale-medium';
 import 'halftone-web-component';
 import '../floatingheader';
 
 export const template = function(scope) { return html`
-    <sp-theme scale="medium" color="light">
+    <div>
         <ht-sidepanel style="background-color: blue; width: 250px; height: 100%;"></ht-sidepanel>
         <div id="main-container">
             <ht-floatingheader></ht-floatingheader>
@@ -15,7 +12,6 @@ export const template = function(scope) { return html`
                   distance="14"
                   shapecolor="white"
                   shapetype="circles">
-                <div id="background" style="background-color: black"></div>
             </halftone-svg>
             <ht-floatingfooter style="background-color: purple; width: 100%; height: 100px;"></ht-floatingfooter>
         </div>
